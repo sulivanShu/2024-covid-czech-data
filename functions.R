897243cb78e49140ac1b045d814761c447c0e3a3
sulivanShu <regivanx@gmail.com>
2025-10-04 00:27:57 +0200

    meilleure conformité lintr

--- functions.R
+++ functions.R
@@ -1,75 +1,73 @@
 load_or_install_then_load_one <- function(pkg) {
-	if (!requireNamespace(pkg, quietly = TRUE)) {
-		install.packages(pkg)
-	}
-	library(pkg, character.only = TRUE)
+  if (!requireNamespace(pkg, quietly = TRUE)) {
+    install.packages(pkg)
+  }
+  library(pkg, character.only = TRUE)
 }
 
 load_or_install_then_load <- function(pkgs) {
-	lapply(pkgs, load_or_install_then_load_one) |>
-		invisible()
+  lapply(pkgs, load_or_install_then_load_one) |>
+    invisible()
 }
 
-downloadAndCheckData <- function(file, url, refhash, quote) {
-	if (!file.exists(file)) {
-		message("\n🛈 Le fichier est absent. Téléchargement en cours...")
-		download.file(url, destfile = file, mode = "wb", quiet = TRUE)
-		message("\n🛈 Vérification de l'intégrité...\n")
-		hash_calc <- digest(file = file, algo = "blake3")
-		if (identical(hash_calc, refhash)) {
-			message("\n✔ Intégrité vérifiée : le hash correspond. On continue.\n")
-		} else {
-			stop("\n❌ Erreur : le hash ne correspond pas. Arrêt du programme.")
-		}
-	} else {
-		message("\n🛈 Fichier déjà téléchargé: pas de vérification du hash.")
-		message("⚠️ Vérifiez vous-même le hash si vous avez téléchargé le fichier manuellement.")
-	}
-	message(paste("\n🛈 Source de données: ", quote))
+download_and_check_data <- function(file, url, refhash, quote) {
+  if (!file.exists(file)) {
+    message("\n🛈 Le fichier est absent. Téléchargement en cours...")
+    download.file(url, destfile = file, mode = "wb", quiet = TRUE)
+    message("\n🛈 Vérification de l'intégrité...\n")
+    hash_calc <- digest(file = file, algo = "blake3")
+    if (identical(hash_calc, refhash)) {
+      message("\n✔ Intégrité vérifiée : le hash correspond. On continue.\n")
+    } else {
+      stop("\n❌ Erreur : le hash ne correspond pas. Arrêt du programme.")
+    }
+  } else {
+    message("\n🛈 Fichier déjà téléchargé: pas de vérification du hash.")
+    message("⚠️ Vérifiez vous-même le hash si vous avez téléchargé le fichier manuellement.")
+  }
+  message(paste("\n🛈 Source de données: ", quote))
 }
 
 load_csv_data <- function(file, with) {
-	fread(file, select = with)
+  fread(file, select = with)
 }
 
-formatData <- function(data) {
-	lapply(data[YEAR_YEAR], \(cell) {
-		ifelse(nchar(cell) == 9, sub("(^....).*", "\\1", cell),NA)  |>
-			paste0("-01-01")  |>
-			as.Date() |>
-			(\(date_start) {
-				interval(date_start, date_start + years(4) + months(11) + days(30))
-			})()
-	}) ->
-		data[YEAR_YEAR]
-	lapply(data[YEAR_WEEK], \(cell) {
-		ifelse(nchar(cell) == 0, NA, cell) ->
-			cell
-		ifelse(nchar(cell) == 7, sub("(.....)(..)", "\\1W\\2-1", cell), cell)  |> 
-			ISOweek2date() |>
-			(\(date_start) interval(date_start, date_start + days(6)))()
-	}) ->
-		data[YEAR_WEEK]
-	data
+formatdata <- function(data) {
+  lapply(data[year_year], \(cell) {
+    ifelse(nchar(cell) == 9, sub("(^....).*", "\\1", cell), NA) |>
+      paste0("-01-01") |>
+      as.Date() |>
+      (\(date_start) {
+        interval(date_start, date_start + years(4) + months(11) + days(30))
+      })()
+  }) ->
+  data[year_year]
+  lapply(data[year_week], \(cell) {
+    ifelse(nchar(cell) == 0, NA, cell) ->
+    cell
+    ifelse(nchar(cell) == 7, sub("(.....)(..)", "\\1W\\2-1", cell), cell) |>
+      ISOweek2date() |>
+      (\(date_start) interval(date_start, date_start + days(6)))()
+  }) ->
+  data[year_week]
+  data
 }
 
-excludeInvalidData <- function(data) {
-	data[
-	(!is.na(int_start(data$birth_year)) &
-		int_start(data$birth_year) >= as.Date("1922-07-01")) &
-		(is.na(int_end(data$date_of_death_registry)) |
-			int_end(data$date_of_death_registry) >= as.Date("2020-12-25")),
-	]
+exclude_invalid_data <- function(data) {
+  data[
+    (!is.na(int_start(data$birth_year)) &
+      int_start(data$birth_year) >= as.Date("1922-07-01")) &
+      (is.na(int_end(data$date_of_death_registry)) |
+        int_end(data$date_of_death_registry) >= as.Date("2020-12-25")),
+  ]
 }
 
-averageDate <- function(data) {
-lapply(data,
-	\(col) vapply(col,
-		\(cell) c(int_start(cell), int_end(cell)) |>
-			mean(),
-		numeric(1)
-		) |>
-		as.POSIXct(origin = "1970-01-01", tz = "UTC")
-) |>
-	as.data.frame()
+average_date <- function(data) {
+  lapply(data, \(col) {
+    vapply(col, \(cell) {
+      mean(c(int_start(cell), int_end(cell)))
+    }, numeric(1)) |>
+      as.POSIXct(origin = "1970-01-01", tz = "UTC")
+  }) |>
+    as.data.frame()
 }
--- main.R
+++ main.R
@@ -1,72 +1,72 @@
 source("variables.R")
 source("functions.R")
 
-load_or_install_then_load(Libraries)
+load_or_install_then_load(libraries)
 
-downloadAndCheckData(CzechDataCsv, CzechDataCsvUrl, CzechDataCsvb3sum, CzechDataCsvQuote)
+download_and_check_data(czech_data_csv, czech_data_csv_url, czech_data_csv_b3sum, czech_data_csv_quote)
 
-load_csv_data(CzechDataCsv, with = MyCzechHeader) ->
-	CzechData
+load_csv_data(czech_data_csv, with = my_czech_header) ->
+czech_data
 
 message("\n🛈 Traduction des entêtes en anglais")
 
-CzechData |>
-	setnames(MyEnglishHeader) ->
-	EnglishCzechData
+czech_data |>
+  setnames(my_english_header) ->
+english_czech_data
 
 message("\n🛈 Contrôle des incohérences...")
 message("\n🛈 Les colonnes sont de type `character` alors que les données sont de type `intervale de dates`. Pour des raisons de praticité et de simplicité, ces colonnes doivent formatées en `date` unique:")
 
-EnglishCzechData |>
-	lapply(class)
+english_czech_data |>
+  lapply(class)
 
 message("\n🛈 Certains individus seraient nés avant 1895 et encore vivants en 2020, ce qui est impossible (record de longévité: 121 ans). Il s'agit d'erreurs de saisies. Par ailleurs, même pour les individus qui seraient nés avant 1920, le risque d'erreur de saisie existe, de sorte qu'un individu né en 2015 pourrait avoir été enregistré comme né en 1915. Par conséquent, les individus enregistrés comme nés avant 1920 seront écartés. Par ailleurs, les individus dont la valeur de naissance est `-`, qui signifie probablement une donnée absente, doivent également être écartés:")
 
-EnglishCzechData$birth_year |>
-	unique() |>
-	sort()
+english_czech_data$birth_year |>
+  unique() |>
+  sort()
 
 message("\n🛈 Certains individus sont déclarés morts avant le début de la campagne de vaccination. Il ne font donc pas l'objet de cette étude et seront donc écartés:")
 
-EnglishCzechData$date_of_death_registry |>
-	unique() |>
-	sort()
+english_czech_data$date_of_death_registry |>
+  unique() |>
+  sort()
 
 message("\n🛈 Pas d'incohérence visible:")
 
-EnglishCzechData$date_dose1 |>
-	unique() |>
-	sort()
+english_czech_data$date_dose1 |>
+  unique() |>
+  sort()
 
 message("\n🛈 Échantillon des données réelles:")
 
-EnglishCzechData |>
-	(\(data) data[,c("infection","birth_year","date_dose1","date_of_death_registry")])() |>
-	head(1000) |> 
-	print() ->
-	MySetOfEnglishCzechData 
+english_czech_data |>
+  (\(data) data[, c("infection", "birth_year", "date_dose1", "date_of_death_registry")])() |>
+  head(1000) |>
+  print() ->
+my_set_of_english_czech_data
 
 
 # message("\n🛈 Données fictives:")
-# DummyData |>
-# 	print() ->
-# 	MySetOfEnglishCzechData 
+# dummy_data |>
+# print() ->
+# my_set_of_english_czech_data
 
 # Données réelles: attention, assez gros
-# MySetOfEnglishCzechData = EnglishCzechData
+# my_set_of_english_czech_data = english_czech_data
 
 message("\n🛈 Formatage et exclusion des données invalides:")
 
-MySetOfEnglishCzechData  |>
-	lapply(class)
+my_set_of_english_czech_data |>
+  lapply(class)
 
-MySetOfEnglishCzechData |>
-	as.data.frame() |>
-	formatData() |>
-	# excludeInvalidData() |> # à vérifier!
-	# averageDate() |> # à corriger!
-	# as.data.table() |>
-	print()
+my_set_of_english_czech_data |>
+  as.data.frame() |>
+  formatdata() |>
+  # exclude_invalid_data() |> # à vérifier!
+  # average_date() |> # à corriger!
+  # as.data.table() |>
+  print()
 
 # https://r-dev-perf.borishejblum.science/parallelisation-du-code-r
 #
