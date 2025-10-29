7b70810653097224ee720d21fc3964a713fe52a9
sulivanShu <regivanx@gmail.com>
2025-10-29 10:30:15 +0100

    Ne pas séparer les vaccinés des non-vaccinés dans subdivide2.jl mais plus tard

--- src/subdivide2.jl
+++ src/subdivide2.jl
@@ -1,6 +1,4 @@
 @info "Splitting data in vector of DataFrames (parallel)"
-df.:vaccinated = .!ismissing.(df.:week_of_dose1)
 df = ThreadsX.map(g -> DataFrame(g),
-									groupby(df, [:vaccinated, :_5_years_cat_of_birth, :sex]))
+									groupby(df, [:_5_years_cat_of_birth, :sex]))
 @info "Splitting completed (parallel)"
-df
