36004048e79bbe2a24c355c2654b8760997656ed
sulivanShu <regivanx@gmail.com>
2025-11-14 10:25:23 +0100

    Rééctriture de indexing.jl sous forme de pipes

--- src/indexing.jl
+++ src/indexing.jl
@@ -1,8 +1,15 @@
 @info "Indexing data"
+
 dfs = Dict(
     parse(Int, string(first(df.sex), first(df."_5_years_cat_of_birth"))) => df
     for df in dfs
 )
-ThreadsX.foreach(subdf -> select!(subdf.second, [:week_of_dose1, :week_of_death]), dfs)
+
+ThreadsX.foreach(subdf -> begin
+									 select!(subdf.second, [:week_of_dose1, :week_of_death])
+									 foreach(col -> replace!(col, missing => Date(10000,1,1)), eachcol(subdf.second))
+									 insertcols!(subdf.second, 1, :available => Date(-10000,1,1))
+								 end, dfs)
+
 @info "Indexing completed"
 
